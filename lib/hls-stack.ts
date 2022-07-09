69e51e7458883f19e0dfc4cbec144d98761ad9c2
Simon Buckle <simon.buckle@gmail.com>
2022-07-09 09:15:36 +0100

    Initial version of stack for creating the S3 bucket as per the example on the website

--- lib/hls-stack.ts
+++ lib/hls-stack.ts
@@ -1,16 +1,42 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Effect } from 'aws-cdk-lib/aws-iam';
 
 export class HlsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'HlsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const corsRule: s3.CorsRule = {
+      allowedMethods: [s3.HttpMethods.GET],
+      allowedOrigins: ['http://hlsbook.net'],
+      allowedHeaders: ['Authorization', 'Range'],
+      maxAge: 3000
+    };
+    const bucket = new s3.Bucket(this, 'HlsBucket', {
+      cors: [corsRule],
+      removalPolicy: RemovalPolicy.DESTROY
+    });
+    bucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ['s3:GetObject'],
+        resources: [bucket.arnForObjects('*')],
+        principals: [new iam.StarPrincipal()],
+        effect: Effect.ALLOW
+      })
+    );
+    bucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ['s3:GetObject'],
+        resources: [bucket.arnForObjects('*')],
+        principals: [new iam.StarPrincipal()],
+        effect: Effect.DENY,
+        conditions: {
+          'StringNotLike': { 'aws:Referer': ['http://hlsbook.net/*'] }
+        }
+      })
+    );
+    new CfnOutput(this, 'Bucket', { value: bucket.bucketName });
   }
 }
