d4dcfc801044a7dcb4bde8bfe4784e385099b3d2
Simon Buckle <simon.buckle@gmail.com>
2022-08-02 08:25:54 +0100

    Added a  parameter

--- lib/hls-stack.ts
+++ lib/hls-stack.ts
@@ -7,9 +7,11 @@ export class HlsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const domain = this.node.tryGetContext('domain') || "*";
+
     const corsRule: s3.CorsRule = {
       allowedMethods: [s3.HttpMethods.GET],
-      allowedOrigins: ['http://hlsbook.net'],
+      allowedOrigins: [domain],
       allowedHeaders: ['Authorization', 'Range'],
       maxAge: 3000
     };
@@ -25,17 +27,19 @@ export class HlsStack extends Stack {
         effect: iam.Effect.ALLOW
       })
     );
-    bucket.addToResourcePolicy(
-      new iam.PolicyStatement({
-        actions: ['s3:GetObject'],
-        resources: [bucket.arnForObjects('*')],
-        principals: [new iam.StarPrincipal()],
-        effect: iam.Effect.DENY,
-        conditions: {
-          'StringNotLike': { 'aws:Referer': ['http://hlsbook.net/*'] }
-        }
-      })
-    );
+    if (domain !== "*") {
+      bucket.addToResourcePolicy(
+        new iam.PolicyStatement({
+          actions: ['s3:GetObject'],
+          resources: [bucket.arnForObjects('*')],
+          principals: [new iam.StarPrincipal()],
+          effect: iam.Effect.DENY,
+          conditions: {
+            'StringNotLike': { 'aws:Referer': [`${domain.replace(/\/+$/, '')}/*`] }
+          }
+        })
+      );
+    }
     new CfnOutput(this, 'Bucket', { value: bucket.bucketName });
   }
 }
